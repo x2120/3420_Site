518bdcf49db74cf52bf68a876af904a1ba7b20ab
mitchell271 <mitchell271@gmail.com>
2015-10-12 14:39:39 -0400

    blah
    blah

--- index.php
+++ index.php
@@ -35,8 +35,9 @@ h1 {
 </style>
 -->
 
-<!-- Header and meta info -->
+<!-- Head and meta info -->
 <head>
+<<<<<<< HEAD:index.php
 	<meta charset="utf-8">
 	<title>Home</title>
 	<meta name="Home page" content="Basic splash home page with information for new uses">
@@ -74,3 +75,71 @@ h1 {
 	</div>
 </body>
 </html>
+=======
+	<link type='text/css' rel='stylesheet' href='general.css' />
+	<div class="toplogo">
+		<img src="movie-reel.jpg" alt="Mountain View" style="width:100px;height:80px;">
+	</div>
+	
+	<div class="tophead">
+		<meta charset="utf-8">
+		<title>Home</title>
+		<meta name="Home page" content="Basic splash home page with information for new uses">
+
+		<h1><span ID='logoColor'>MyVid</span> Collection</h1>
+		<p>An individual Video Libary</p><br>
+	</div>
+<head>
+
+
+<div class='center'>
+
+	<!-- Left sidebar and site navigation -->
+	<div class='left'>
+		<br>
+		<ul>
+			<a href="index.html"><li class='button'>Home</li></a>
+			<a href="register.html"><li class='button'>Create Account</li></a>
+			<a href="profile.html"><li class='button'>Edit Account</li></a>
+			<a href="leave.php"><li class='button'>Delete Account</li></a>
+			<a href="addVid.html"><li class='button'>Add Video</li></a>
+			<a href="editVid.html"><li class='button'>Edit Video</li></a>
+			<a href="deleteVid.php"><li class='button'>Delete Video</li></a>
+			<a href="search.html"><li class='button'>Search</li></a>
+			<a href="displayDetails.html"><li class='button'>Display Details</li></a>
+			<a href="login.html"><li class='button'>Login</li></a>
+			<a href="logout.php"><li class='button' ID='inv_button'>Logout</li></a>
+		</ul>
+	</div>
+
+</div>
+
+<div class="main">
+    Main body of the file
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	</p>
+	
+</div>
+>>>>>>> parent of bf4a54a... Maor updates:index.html
