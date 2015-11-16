a9f35b85f61b3d583d5329688ea7ac68cf9c6c77
mitchell271 <mitchell271@gmail.com>
2015-11-16 14:24:10 -0500

    Login changes and scripts

--- Pages/login.php
+++ Pages/login.php
@@ -18,28 +18,37 @@
 
 	<div class = 'right'>
 		<div class = 'display_box' id = 'pretty_box'>
-			<ul>
-				<li>
-					<div class = 'box_title'>
-						Username:
-					</div>
-					<div class = 'box_field'>
-						<input type="text" value ="" name="UserName" />
-					</div>
-				</li>
-				<li>
-					<div class = 'box_title'>
-						Password:
-					</div>
-					<div class = 'box_field'>
-						<input type="password" value ="" name="Password" />
-					</div>
-				</li>
-			</ul>
-			<input type="Button" value ="Login" name="login" />
-			<input type="RESET" value ="Reset" name="reset" />
+			<form name = "login" action = "../Scripts/login_script.php" method = "post" enctype = "multipart/form-data">
+				<ul>
+					<li>
+						<div class = 'box_title'>
+							Username:
+						</div>
+						<div class = 'box_field'>
+							<input type = "text" value = "" name = "username" />
+						</div>
+					</li>
+					<li>
+						<div class = 'box_title'>
+							Password:
+						</div>
+						<div class = 'box_field'>
+							<input type = "password" value = "" name = "password" />
+						</div>
+					</li>
+				</ul>
+				<input type="Button" value ="Login" name="login" />
+				<input type="RESET" value ="Reset" name="reset" />
+			</form>
 		</div>
 	</div>
 </div>
+
+<?php
+	// cookie and session creation
+	$cookie_name = "user"
+	$cookie_type = "$UserName"
+?>
+
 </body>
 </html>
