87d4de4aa4658e6f875918d1340fb077f1fa654d
mitchell271 <mitchell271@gmail.com>
2015-11-21 17:09:24 -0500

    ARLGANFJAVUOVBNASDVN

--- Scripts/login_script.php
+++ Scripts/login_script.php
@@ -1,5 +1,5 @@
 <?php
-    $servername = "localhost";
+ 	$servername = "localhost";
 	$server_username = "justinvuong";
 	$server_password = "1234";
 	$dbname = "justinvuong";
@@ -22,40 +22,49 @@
 	$client_username = mysql_real_escape_string($client_username);
 	$client_password = mysql_real_escape_string($client_password);
 
-	$sql = "SELECT * FROM User WHERE name = '$client_username' and password = '$client_password' ";
+	$sql = "SELECT * FROM User WHERE name = '$client_username' and password = '$client_password'";
 
-	$result = mysql_query($sql);
-	$count = mysql_num_rows($result);
+	$result = mysql_query($sql) or trigger_error(mysql_error()." ".$sql);
 
-	//if results match, then count = 1
-	if($count == 1)
+	if ($result == false)
 	{
-		//session creation
-		$session_name = 'sec_session_id';
-	    $secure = SECURE;
-	        
-	    // Forces sessions to only use cookies.
-	    if (ini_set('session.use_only_cookies', 1) === FALSE)
-	    {
-	        header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
-	        exit();
-	    }
-	    
-	    // Gets current cookies params.
-	    setcookie('Username', $client_username, time() + (86400 * 30), "/");
-	    
-	    // Sets the session name to the one set above.
-	    session_name($session_name);
-	    session_start();
-	    session_regenerate_id(true);
-
-	    echo "<p>Login successful</p>";
+		echo "<p>Query Error</p>";
+		mysql_close($conn);
 	}
 	
 	else
-		{ echo "Wrong Username or Password"; }
+	{ 
+		$count = mysql_num_rows($result); 
+		//if results match, then count = 1
+		if($count == 1)
+		{
+			//session creation
+			$session_name = 'sec_session_id';
+		    $secure = SECURE;
+		        
+		    // Forces sessions to only use cookies.
+		    if (ini_set('session.use_only_cookies', 1) === FALSE)
+		    {
+		        header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+		        exit();
+		    }
+		    
+		    // Gets current cookies params.
+		    setcookie('Username', $client_username, time() + (86400 * 30), "/");
+		    
+		    // Sets the session name to the one set above.
+		    session_name($session_name);
+		    session_start();
+		    session_regenerate_id(true);
 
-	mysql_close($conn);
+		    echo "<p>Login successful</p>";
+		}
+		
+		else
+			{ echo "Wrong Username or Password"; }
+
+		mysql_close($conn);
+	}
 ?>
     
     <!-- //check if logged in
