ea19f0db9c6e222fb2276feece5ca7ceb7cb62d6
x2120 <tinvou@gmail.com>
2015-11-22 02:21:39 -0500

    stuff from Mitch?
    he could not commit :(

--- Pages/login.php
+++ Pages/login.php
@@ -1,52 +1,9 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
-<!-- Head and meta info -->
-<head>
-	<link type='text/css' rel='stylesheet' href='../CSS/general.css' />
-	<meta charset="utf-8">
-	<title>Login</title>
-	<meta name="User login" content="The login page for the website">
-</head>
-
-<body>
-<div class='center'>
-
-	<?php
-		require "../Templates/header.php"; 
-		require "../Templates/sidebar.php"; 
-	?>
-
-	<div class = 'right'>
-		<div class = 'display_box' id = 'pretty_box'>
-			<form name = "login" action = "../Scripts/login_script.php" method = "post" enctype = "multipart/form-data">
-				<ul>
-					<li>
-						<div class = 'box_title'>
-							Username:
-						</div>
-						<div class = 'box_field'>
-							<input type = "text" value = "" name = "client_username" />
-						</div>
-					</li>
-					<li>
-						<div class = 'box_title'>
-							Password:
-						</div>
-						<div class = 'box_field'>
-							<input type = "password" value = "" name = "client_password" />
-						</div>
-					</li>
-				</ul>
-				<input type="submit" value ="Login" name="login" />
-				<input type="RESET" value ="Reset" name="reset" />
-			</form>
-		</div>
-	</div>
-</div>
-
 <?php
+require_once "../Scripts/login_script.php";
+$login = new Login();
 
+if ($login->login_check() == true)
+    { include("index.php"); } 
+else
+    { include("not_logged.php"); }
 ?>
-
-</body>
-</html>
