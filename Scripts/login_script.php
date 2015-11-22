20556faba2b2e23c216f2ed3eb82fb220fc243e1
mitchell271 <mitchell271@gmail.com>
2015-11-21 21:04:46 -0500

    fuck thIS SHIT
    (ノಠ益ಠ)ノ彡┻━┻

--- Scripts/login_script.php
+++ Scripts/login_script.php
@@ -1,78 +1,103 @@
 <?php
- 	$servername = "localhost";
-	$server_username = "justinvuong";
-	$server_password = "1234";
-	$dbname = "justinvuong";
+class Login
+{
+	public $conn = null;
+	public $errors = array();
+	public $messages = array();
 
-	// Create connection
-	$conn = mysql_connect($servername, $server_username, $server_password, $dbname);
-	if ($conn == true)
-		{ echo "<p>Woot it's working</p>"; }
-
-	// Check connection
-
-	if ($conn == 0)
-		{ die("Connection failed: " . $conn->connect_error); }
-	
-	$client_username = isset($_POST["client_username"]);
-	$client_password = isset($_POST["client_password"]);
-
-	$client_username = stripslashes($client_username);
-	$client_password = stripslashes($client_password);
-	$client_username = mysql_real_escape_string($client_username);
-	$client_password = mysql_real_escape_string($client_password);
-
-	$sql = "SELECT * FROM User WHERE name = '$client_username' and password = '$client_password'";
-
-	$result = mysql_query($sql) or trigger_error(mysql_error()." ".$sql);
-
-	if ($result == false)
+	public function __construct()
 	{
-		echo "<p>Query Error</p>";
-		mysql_close($conn);
-	}
-	
-	else
-	{ 
-		$count = mysql_num_rows($result); 
-		//if results match, then count = 1
-		if($count == 1)
-		{
-			//session creation
-			$session_name = 'sec_session_id';
-		    $secure = SECURE;
-		        
-		    // Forces sessions to only use cookies.
-		    if (ini_set('session.use_only_cookies', 1) === FALSE)
-		    {
-		        header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
-		        exit();
-		    }
-		    
-		    // Gets current cookies params.
-		    setcookie('Username', $client_username, time() + (86400 * 30), "/");
-		    
-		    // Sets the session name to the one set above.
-		    session_name($session_name);
-		    session_start();
-		    session_regenerate_id(true);
-
-		    echo "<p>Login successful</p>";
+		session_start();
+		if (isset($_POST["login"]))
+		{ 
+			$this->login_function();			
 		}
-		
-		else
-			{ echo "Wrong Username or Password"; }
+	}	
 
-		mysql_close($conn);
+	public function login_function()
+	{
+		echo "<p>Wu-tang</p>";
+
+		$servername = "localhost";
+		$username = "justinvuong";
+		$password = "1234";
+		$dbname = "justinvuong";
+		
+
+		if (!empty($_POST['client_username']) && !empty($_POST['client_password']))
+		{
+			echo "<p>Wu-tang</p>";
+			// Create connection
+			$this->conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error)
+				{ die('Connect Error (' . $conn->connect_errno . ') '. $conn->connect_error); }
+			else
+			{
+				$client_username = isset($_POST["client_username"]);
+				$client_password = isset($_POST["client_password"]);
+
+				$client_username = stripslashes($client_username);
+				$client_password = stripslashes($client_password);
+				$client_username = mysqli_real_escape_string($client_username);
+				$client_password = mysqli_real_escape_string($client_password);
+
+				$sql = "SELECT * FROM User WHERE name = '$client_username' and password = '$client_password'";
+				$result = mysqli_query($sql) or trigger_error(mysqli_error()." ".$sql);
+			}
+
+			if ($result->num_rows == 1)
+			{ echo "<p>Woot</p>"; }
+		}
 	}
+
+	public function login_check()
+	{
+		if (isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] == 1)
+			{ return true; }
+		else
+			{ return false; }
+	}
+}
+	
+
+	// if ($result == false)
+	// {
+	// 	echo "<p>Query Error</p>";
+	// 	mysql_close($conn);
+	// }
+	
+	// else
+	// { 
+	// 	$count = mysql_num_rows($result); 
+	// 	//if results match, then count = 1
+	// 	if($count == 1)
+	// 	{
+	// 		//session creation
+	// 		$session_name = 'sec_session_id';
+	// 		$secure = SECURE;
+				
+	// 		// Forces sessions to only use cookies.
+	// 		if (ini_set('session.use_only_cookies', 1) === FALSE)
+	// 		{
+	// 			header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+	// 			exit();
+	// 		}
+			
+	// 		// Gets current cookies params.
+	// 		setcookie('Username', $client_username, time() + (86400 * 30), "/");
+			
+	// 		// Sets the session name to the one set above.
+	// 		session_name($session_name);
+	// 		session_start();
+	// 		session_regenerate_id(true);
+
+	// 		echo "<p>Login successful</p>";
+	// 	}
+		
+	// 	else
+	// 		{ echo "Wrong Username or Password"; }
+
+	// 	mysql_close($conn);
+	// }
 ?>
-    
-    <!-- //check if logged in
-    function login_check()
-    {
-    if (isset($_SESSION['user_login_status']) && $_SESSION['user_login_status'] == 1)
-    { return true; }
-      
-    else
-    { return false; }
-	} -->
--- php-login-minimal-master/classes/Registration.php
+++ php-login-minimal-master/classes/Registration.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class registration
  * handles the user registration
@@ -17,6 +18,7 @@ class Registration
      * @var array $messages Collection of success / neutral messages
      */
     public $messages = array();
+
     /**
      * the function "__construct()" automatically starts whenever an object of this class is created,
      * you know, when you do "$registration = new Registration();"
@@ -27,6 +29,7 @@ class Registration
             $this->registerNewUser();
         }
     }
+
     /**
      * handles the entire registration process. checks all error possibilities
      * and creates a new user in the database if everything is fine
@@ -64,23 +67,30 @@ class Registration
         ) {
             // create a database connection
             $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
             // change character set to utf8 and check it
             if (!$this->db_connection->set_charset("utf8")) {
                 $this->errors[] = $this->db_connection->error;
             }
+
             // if no connection errors (= working database connection)
             if (!$this->db_connection->connect_errno) {
+
                 // escaping, additionally removing everything that could be (html/javascript-) code
                 $user_name = $this->db_connection->real_escape_string(strip_tags($_POST['user_name'], ENT_QUOTES));
                 $user_email = $this->db_connection->real_escape_string(strip_tags($_POST['user_email'], ENT_QUOTES));
+
                 $user_password = $_POST['user_password_new'];
+
                 // crypt the user's password with PHP 5.5's password_hash() function, results in a 60 character
                 // hash string. the PASSWORD_DEFAULT constant is defined by the PHP 5.5, or if you are using
                 // PHP 5.3/5.4, by the password hashing compatibility library
                 $user_password_hash = password_hash($user_password, PASSWORD_DEFAULT);
+
                 // check if user or email address already exists
                 $sql = "SELECT * FROM users WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_email . "';";
                 $query_check_user_name = $this->db_connection->query($sql);
+
                 if ($query_check_user_name->num_rows == 1) {
                     $this->errors[] = "Sorry, that username / email address is already taken.";
                 } else {
@@ -88,6 +98,7 @@ class Registration
                     $sql = "INSERT INTO users (user_name, user_password_hash, user_email)
                             VALUES('" . $user_name . "', '" . $user_password_hash . "', '" . $user_email . "');";
                     $query_new_user_insert = $this->db_connection->query($sql);
+
                     // if user has been added successfully
                     if ($query_new_user_insert) {
                         $this->messages[] = "Your account has been created successfully. You can now log in.";
