b167efb367b8bd8978ffab1446d438d28ed4e908
x2120 <tinvou@gmail.com>
2015-11-22 01:46:27 -0500

    Editing
    Can edit files properly
    reltype and cover still need changes

--- Scripts/change.php
+++ Scripts/change.php
@@ -14,9 +14,9 @@ if ($conn->connect_error) {
 }
 
 $MTitle=($_GET['MTitle']); 
-$MRate=($_GET['Rating[]']); 
+$MRate=($_GET['Rating']); 
 $Genre=($_GET['Genre']); 
-$SRate=($_GET['Rating']); 
+$SRate=($_GET['SRating']); 
 $Year=($_GET['Year']); 
 $Runtime=($_GET['Runtime']); 
 $TRelease=($_GET['TRelease']); 
@@ -25,14 +25,15 @@ $Actors=($_GET['Actors']);
 $Save=($_GET['AddVideo']); // to upload image  
 $Studio=($_GET['Studio']); 
 $Plot=($_GET['Plot']); 
-$Vtype=($_GET['VType[]']); 
-
+$VType=($_GET['type']); 
 
 // sql to insert into table 																							add cover (upload image)
-$sql = "DELETE FROM Movie WHERE title = '$MTitle' ";
 
-$sql = "INSERT INTO Movie (title, genre, actors, MPAA, relType, stars, runtime, theatRelease, homeRelease, studio, plot)
-VALUES ('$MTitle','$Genre','$Actors','$MRate','$Vtype','$SRate','$Runtime','$TRelease','$DRelease','$Studio','$Plot') "//add cover
+
+$sql = "UPDATE Movie 
+SET genre='$Genre', actors='$Actors' , MPAA='$MRate', relType='$Vtype', stars='$SRate', runtime='$Runtime', theatRelease='$TRelease', homeRelease='$DRelease', studio='$Studio', plot='$Plot' 
+WHERE title='$MTitle';
+"//add cover
 ;
 
 if ($conn->query($sql) === TRUE) {
